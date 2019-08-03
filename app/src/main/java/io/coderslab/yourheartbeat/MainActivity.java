a9f153e18b1f51f193f41852d4767b75b2fe1ad0
Bishnu Dev <bishnu.dev52@gmail.com>
2019-08-03 13:15:24 +0545

    Adding bloog group spinner

--- app/src/main/java/io/coderslab/yourheartbeat/MainActivity.java
+++ app/src/main/java/io/coderslab/yourheartbeat/MainActivity.java
@@ -3,6 +3,8 @@ package io.coderslab.yourheartbeat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_blood_grp);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.bloog_grp_array, R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
     }
 }
