1f1b116a80c6ef1e5f3a8d7e50ae096b444354ee
Bishnu Dev <bishnu.dev52@gmail.com>
2019-08-03 17:08:47 +0545

    launchscreen ui updated

--- app/src/main/java/io/coderslab/yourheartbeat/MainActivity.java
+++ app/src/main/java/io/coderslab/yourheartbeat/MainActivity.java
@@ -1,8 +1,12 @@
 package io.coderslab.yourheartbeat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -14,9 +18,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Spinner spinner = (Spinner) findViewById(R.id.spinner_blood_grp);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.bloog_grp_array, R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.blood_grp_array, R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spinner.setAdapter(adapter);
+
+
+        ConstraintLayout registerWrapper = (ConstraintLayout) findViewById(R.id.registerformbg);
+        registerWrapper.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (MotionEvent.ACTION_DOWN == event.getAction()){
+                    setElevation(v, 0);
+                }
+                if (MotionEvent.ACTION_UP == event.getAction()){
+                    setElevation(v, 12);
+                }
+                return true;
+            }
+        });
+
+    }
+
+    public void setElevation(View v, int units){
+        int apiLevel = Build.VERSION.SDK_INT;
+        if (apiLevel >= 21){
+            v.setElevation(units);
+        }
     }
 }
